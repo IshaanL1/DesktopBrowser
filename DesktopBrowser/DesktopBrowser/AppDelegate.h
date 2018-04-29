2ea1b0aa7e7369addac388ad33b9ab4d5b128656
Jeffrey Bergier <jeffburg@jeffburg.com>
2018-04-29 17:56:24 +0700

    initial commit

---
+++ DesktopBrowser/DesktopBrowser/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DesktopBrowser
+//
+//  Created by Jeffrey Bergier on 29/04/2018.
+//  Copyright © 2018 Jeffrey Bergier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DesktopBrowser/DesktopBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DesktopBrowser
+//
+//  Created by Jeffrey Bergier on 29/04/2018.
+//  Copyright © 2018 Jeffrey Bergier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
